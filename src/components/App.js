2706b7786371d9b6028b7325251e2212604c0f51
Jefferson Brito <jefersonbritoalm@gmail.com>
2024-06-19 22:04:19 -0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Login from "../pages/Login/Login";
+import BackgroundNew from "../assets/images/bg-mobile.svg";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Navbar from "./ui/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/app" element={<><Navbar/> <Dashboard /></>} />
+        </Routes>
+      </BrowserRouter>
+      <div
+        className='app-bg'
+      />
+    </div>
+  );
+}
+
+export default App;
